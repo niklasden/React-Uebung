f8fb8b1c45d8547035c44cbe235f4ab0c4dced1a
niklasden <niklas.denneler@googlemail.com>
2020-01-23 17:44:21 +0100

    Aufgabe 3) erledigt

--- React 3 Übung/react3_2/react3_2/src/Posts.js
+++ React 3 Übung/react3_2/react3_2/src/Posts.js
@@ -2,12 +2,27 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import { Divider } from '@material-ui/core'
+import PostItem from './PostItem'
 
 /**
  * Beinhaltet alle PostItems und die Logik hinter diesen. Ermoeglicht eine Filterung nach Titel und Beschreibung der einzelnen PostItems
  * @see PostItem
  */
 class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          posts: [],
+        };
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+            this.setState({posts})}
+        ); 
+    }
+    
     render() {
         return (
             <div >
@@ -27,6 +42,7 @@ class Posts extends React.Component {
                 <Divider variant='fullWidth'></Divider>
                 {
                     /** Hier Implementation des Renderings der PostItems */
+                    this.state.posts.map(post => (<PostItem key={post.id} title={post.title} img={"https://picsum.photos/690"} url="https://pornhub.com/juliussextape" description={post.title}></PostItem>))
                 }
             </div>
         );
